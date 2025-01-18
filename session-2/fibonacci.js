2811f3b5b9058e0ae8f7a1b84481380935a1ecd1
Alex Scripcariu <alexscripcariu41209@gmail.com>
2025-01-18 06:11:40 +0000

    made the fibonacci function return an array instead of just console.log()

--- session-2/fibonacci.js
+++ session-2/fibonacci.js
@@ -1,22 +1,25 @@
 // Code to make a fibonacci sequence
 
 function fibonacciSequence(maxCount) {
+  let ans = [];
   let currentSum = 0;
   let prevNum = 1;
   let prevSum = 0;
 
   for (let count = 1; count <= maxCount; count++) {
     if (count === 1) {
-      console.log(`Fibonacci num ${count}: ${currentSum}`);
+      ans.push(0);
       continue;
     }
 
     currentSum = prevNum + prevSum;
-    console.log(`Fibonacci num ${count}: ${currentSum}`);
+    ans.push(currentSum);
     prevNum = prevSum;
     prevSum = currentSum;
   }
 
+  return ans;
 }
+
 
 fibonacciSequence(10);
